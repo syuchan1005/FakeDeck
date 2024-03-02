38d29959003ffcbe2bce8c67e30d5b9d7f3eca5b
syuchan1005 <syuchan.dev@gmail.com>
2024-03-02 17:41:54 +0900

    Add Encoders class

--- src/main.ino
+++ src/main.ino
@@ -4,9 +4,11 @@
 #include "./usb_descriptors.h"
 
 #include "./input/buttons.cpp"
+#include "./input/encoder.h"
 
 // TODO: Supports button array
 Buttons buttons((uint8_t[]){3, 7, 11, 15, 16});
+Encoders encoders((uint8_t[]){17, 18});
 
 uint8_t const desc_hid_report[] = {TUD_HID_REPORT_DESC()};
 
@@ -27,6 +29,7 @@ uint8_t serial[20] = {0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A
 void setup()
 {
     buttons.init();
+    encoders.init();
 
     TinyUSBDevice.setManufacturerDescriptor(DECK_USB_MANUFACTURER);
     TinyUSBDevice.setProductDescriptor(DECK_USB_PRODUCT);
@@ -49,9 +52,11 @@ void loop()
 {
     delay(100);
 
-    uint8_t report[INPUT_REPORT_LEN] = {0x00, KEY_COUNT, 0x00};
+    uint8_t report[INPUT_REPORT_LEN] = {0x00, KEY_COUNT, 0x00}; // key, count, ?
     buttons.getButtons(report, 3);
     usb_hid.sendReport(1, report, sizeof(report));
+
+    maybeSendDialReport();
 
     /*
     uint8_t touchscreenReport[INPUT_REPORT_LEN] = {
@@ -62,18 +67,36 @@ void loop()
     };
     See more: py\Lib\site-packages\StreamDeck\Devices\StreamDeckPlus.py#L356
     */
+}
 
-    /*
+std::vector<int8_t> dialValues;
+void maybeSendDialReport() {
+    bool isDialChanged = false;
+    std::vector<int8_t> newDialValues = encoders.getEncoderValues();
     uint8_t dialReport[INPUT_REPORT_LEN] = {
         0x03, DIAL_COUNT, 0x00, // dial, count, ?
-        0x00, // 1turn(val) 0push(bool)
-        0x01, // dials
-        0x00,
-        0x00,
-        0x00,
+        0x01,                   // 1turn(val) 0push(bool)
+        0x00,                   // dials
     };
-    See more: py\Lib\site-packages\StreamDeck\Devices\StreamDeckPlus.py#L371
-    */
+    if (dialValues.size() != newDialValues.size()) {
+        for (uint8_t i = 0; i < newDialValues.size(); i++)
+        {
+            dialReport[i + 4] = newDialValues[i];
+        }
+        isDialChanged = true;
+    } else if (dialValues != newDialValues)
+    {
+        for (uint8_t i = 0; i < newDialValues.size(); i++)
+        {
+            dialReport[i + 4] = newDialValues[i] - dialValues[i];
+        }
+        isDialChanged = true;
+    }
+    if (isDialChanged) {
+        dialValues = newDialValues;
+        usb_hid.sendReport(1, dialReport, sizeof(dialReport));
+        Serial.printf("Dial Changed: %d\n", (int8_t) dialReport[4]);
+    }
 }
 
 uint16_t get_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen)
